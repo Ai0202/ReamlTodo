190354248704f641262268fa032ff3639f7af68a
Atsushi <atsushi@ikedaatsushinoMacBook-puro.local>
2018-09-17 15:03:00 +0800

    tableView作成

--- RealmTodo/AddViewController.swift
+++ RealmTodo/AddViewController.swift
@@ -1,10 +1,4 @@
-//
-//  AddViewController.swift
-//  RealmTodo
-//
-//  Created by Atsushi on 2018/09/17.
-//  Copyright © 2018年 Atsushi. All rights reserved.
-//
+
 
 import UIKit
 
@@ -14,25 +8,14 @@ class AddViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
     @IBAction func tapBtn(_ sender: UIButton) {
+        
+        //navigationControllerを使った時に元の画面に戻る書き方
+        self.navigationController?.popViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
